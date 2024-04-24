f04d15fe720d5fa5c56831040d9eeb14e5b7d306
Daniel Oliva <olivaf.daniel@gmail.com>
2024-04-24 15:29:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
 
-function App() {
+import { Droppable } from "./Droppable";
+import { Draggable } from "./Draggable";
+
+export default function App() {
+  const [playersIn, setPlayersIn] = React.useState(["Izzy"]);
+  const [playersOut, setPlayersOut] = React.useState([]);
+  // const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DndContext onDragEnd={handleDragEnd}>
+      <div>
+        <h2>IN</h2>
+        <Droppable id="in">
+          {playersIn.map((element) => (
+            <Draggable key={element} id={element}>
+              {element}
+            </Draggable>
+          ))}
+          {playersIn.length === 0 ? "drop here" : null}
+        </Droppable>
+      </div>
+      <div>
+        <h2>OUT</h2>
+        <Droppable id="out">
+          {playersOut.map((element) => (
+            <Draggable key={element} id={element}>
+              {element}
+            </Draggable>
+          ))}
+          {playersOut.length === 0 ? "drop here" : null}
+        </Droppable>
+      </div>
+    </DndContext>
   );
-}
 
-export default App;
+  function handleDragEnd(event) {
+    const { over, active } = event;
+    if (over?.id === "out") {
+      setPlayersOut((current) => [...current, active.id]);
+      setPlayersIn((current) => current.filter((name) => name !== active.id));
+    }
+
+    if (over?.id === "in") {
+      setPlayersIn((current) => [...current, active.id]);
+      setPlayersOut((current) => current.filter((name) => name !== active.id));
+    }
+  }
+}
